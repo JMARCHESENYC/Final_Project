637754a2d3ad5d09f2beb7db8d1f287350644ba1
JMARCHESENYC <justinmarchese11@gmail.com>
2015-12-16 22:42:22 -0500

    User Profiles

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   root 'posts#index' 
 
   get ':user_name', to: 'profiles#show', as: :profile
-
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
+  
 
   resources :posts do  
   resources :comments
